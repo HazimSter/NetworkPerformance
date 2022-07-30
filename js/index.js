85058f47bd41f2f4148f71f4c6a4ab3d3276e7e1
HazimSter <110282201+HazimSter@users.noreply.github.com>
2022-07-30 16:43:59 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -18,26 +18,26 @@ submitButton.onclick = e => {
    
     
     if (time.value === '8') {
-        userImage.src = '../img/result/' + '8_10.PNG';
+        userImage.src = '/img/result/' + '8_10.PNG';
         paragraph.textContent= 'The best online meeting on this time is Webex'
         hyperLink.textContent= 'visit webex website'
         hyperLink.href= 'https://www.webex.com/ '
         
 
     } else if (time.value === '10') {
-        userImage.src = '../img/result/' + '10_12.PNG';
+        userImage.src = '/img/result/' + '10_12.PNG';
         paragraph.textContent= 'The best online meeting on this time is Zoom'
         hyperLink.textContent= 'visit Zoom website'
         hyperLink.href= 'https://zoom.us/'
 
     } else if (time.value === '2') {
-        userImage.src = '../img/result/' + '2_4.PNG';
+        userImage.src = '/img/result/' + '2_4.PNG';
         paragraph.textContent= 'The best online meeting on this time is Google Meet'
         hyperLink.textContent= 'visit Google Meet website'
         hyperLink.href= 'https://apps.google.com/meet/'
 
     } else if (time.value === '4') {
-        userImage.src = '../img/result/' + '4_6.PNG';
+        userImage.src = '/img/result/' + '4_6.PNG';
         paragraph.textContent= 'The best online meeting on this time is Microsoft Teams'
         hyperLink.textContent= 'visit Microsoft Team website'
         hyperLink.href= 'https://www.microsoft.com/en-us/microsoft-teams/group-chat-software '
